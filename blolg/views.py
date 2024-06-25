14eb06ca466a87a0441409100e5cc97d32bcab48
Иброхим Раджаббоев <ibrohimrajabboyev6000@gmail.com>
2024-06-25 17:08:45 +0500

    First commit

--- blolg/views.py
+++ blolg/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 # Create your views here.
 
 
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='PB')
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blolg/post/list.html'
 
 def post_list(request):
     post_list = Post.objects.filter(status='PB')
-    paginator = Paginator(post_list, 2)
+    paginator = Paginator(post_list, 4)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blolg/post/list.html', {'posts': posts})
+
 
 
 
