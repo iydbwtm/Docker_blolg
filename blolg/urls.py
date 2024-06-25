32122067f1226138db33617254a5b60b3bbe9eae
Иброхим Раджаббоев <ibrohimrajabboyev6000@gmail.com>
2024-06-25 17:32:29 +0500

    First commit

--- blolg/urls.py
+++ blolg/urls.py
@@ -4,6 +4,6 @@ from .views import post_list, PostListView, post_detail
 app_name = 'blolg'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('', post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
