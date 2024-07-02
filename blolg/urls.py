46ed788303b460a7c3b9e9848ab801b5fe174212
Иброхим Раджаббоев <ibrohimrajabboyev6000@gmail.com>
2024-07-02 17:04:37 +0500

    First commit

--- blolg/urls.py
+++ blolg/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_list, PostListView, post_detail, post_share
+from .views import post_list, PostListView, post_detail, post_share, post_comment
 
 app_name = 'blolg'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
+    path('<int:post_id>/comment/', post_comment, name='post_comment'),
 ]
