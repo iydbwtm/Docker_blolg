1b88d430908935f33bd83805328bcf1ece017539
Иброхим Раджаббоев <ibrohimrajabboyev6000@gmail.com>
2024-07-04 18:47:37 +0500

    First commit

--- blolg/models.py
+++ blolg/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+from django.db.models import Count
 #from .managers  import PublishManager
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
 
@@ -21,8 +26,10 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    tags = TaggableManager()
 
-
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
--- blolg/views.py
+++ blolg/views.py
@@ -6,11 +6,13 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 
 class PostListView(ListView):
-    queryset = Post.objects.filter(status='PB')
+    queryset = Post.published.all
     context_object_name = 'posts'
     paginate_by = 4
     template_name = 'blolg/post/list.html'
@@ -35,8 +37,12 @@ def post_share(request, post_id):
     return render(request, 'blolg/post/share.html', {'post': post, 'form': form})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.objects.filter(status='PB')
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 4)
     page_number = request.GET.get('page', 1)
     try:
@@ -45,14 +51,17 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blolg/post/list.html', {'posts': posts})
+    return render(request, 'blolg/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     comments = post.comments.filter(active=True)
-    form = CommentForm() 
-    return render(request, 'blolg/post/detail.html',{'post': post, 'comments': comments, 'form': form})
+    form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request, 'blolg/post/detail.html',{'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 @require_POST
